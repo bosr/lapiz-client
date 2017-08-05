fef4395f9ce0983770dfcb2a411eb220d15a21a9
Romain Bossart <romain.bossart@fastmail.com>
2017-08-05 08:50:46 +0200

    [packaging] add crayon lua client code for later

---
+++ to-migrate/python_old/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pycrayon',
+      description='Crayon client for python',
+      author='torrvision',
+      url='https://github.com/torrvision/crayon',
+      packages=['pycrayon'],
+      version='0.5',
+      install_requires=[
+        "requests"
+      ]
+     )
